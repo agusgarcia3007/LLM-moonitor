62b91ed84fbb8394c893d113b2cb384bdb276580
agustin garcia <agusgarcia3007@gmail.com>
2025-07-13 22:17:26 -0300

    refactor: enhance DashboardLayout with SiteHeader integration
    - Added SiteHeader component to DashboardLayout for improved navigation and user experience.
    - Updated header text to reflect the addition of the SiteHeader, enhancing the overall layout.

--- apps/client/src/components/layout/dashboard/index.tsx
+++ apps/client/src/components/layout/dashboard/index.tsx
@@ -1,16 +1,26 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet } from "@tanstack/react-router";
+import { AppSidebar } from "./app-sidebar";
 import { SiteHeader } from "./site-header";
 
 export function DashboardLayout() {
   return (
     <div>
-      <SidebarProvider>
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" />
         <SidebarInset>
           <SiteHeader />
-          <div className="p-4">
-            <h1>Basic Dashboard with Sidebar + Header</h1>
-            <Outlet />
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              <Outlet />
+            </div>
           </div>
         </SidebarInset>
       </SidebarProvider>
