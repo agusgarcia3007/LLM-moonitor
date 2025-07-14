3461c91248370d9adf240bf3f2e3c19781f1d005
agustin garcia <agusgarcia3007@gmail.com>
2025-07-13 21:41:42 -0300

    refactor: enhance DashboardLayout with Sidebar integration
    - Integrated SidebarProvider and SidebarInset into DashboardLayout for improved layout structure.
    - Updated header to reflect the inclusion of the sidebar, enhancing the overall user interface.

--- apps/client/src/components/layout/dashboard/index.tsx
+++ apps/client/src/components/layout/dashboard/index.tsx
@@ -1,10 +1,17 @@
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet } from "@tanstack/react-router";
 
 export function DashboardLayout() {
   return (
     <div>
-      <h1>Basic Dashboard</h1>
-      <Outlet />
+      <SidebarProvider>
+        <SidebarInset>
+          <div className="p-4">
+            <h1>Basic Dashboard with Sidebar</h1>
+            <Outlet />
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
     </div>
   );
 }
